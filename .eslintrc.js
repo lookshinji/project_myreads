ead3819d5f41981ef074cb5f02563b38113e7234
Julia Caldas <look.shinji@gmail.com>
2017-08-29 10:28:15 -0300

    Uploaded BooksAPI.js file
    Upload of BooksAPI.js file to integratet with Udacity API, necessary to this project.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "extends": ["react-app", "plugin:jsx-a11y/recommended"],
+    "plugins": [
+        "jsx-a11y"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+};
